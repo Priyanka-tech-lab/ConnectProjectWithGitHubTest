836209148bcb5d11b67d2d4e273dd251463c5de7
Priyanka-tech-lab <trivedipriyanka001@gmail.com>
2025-12-02 11:40:11 +0530

    Add RegFormTest page and update layout navigation

---
+++ ConnectProjectWithGitHubTest/Pages/RegFormTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConnectProjectWithGitHubTest.Pages
+{
+    public class RegFormTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
